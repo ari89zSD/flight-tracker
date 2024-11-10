eeb14928b69b38cc5b3c30fb441e1b9469cbcc68
ari89zSD <arifzsidd@gmail.com>
2024-11-09 19:13:53 -0500

    integrated cesium and got it to compile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { CesiumDirective } from './cesium.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CesiumDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'flight-tracker';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Ion } from 'cesium';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+(window as Record<string, any>)['CESIUM_BASE_URL'] = '/assets/cesium/';
+
+// Uncomment the following line and add your personal access token if you are using Cesium Ion
+//Ion.defaultAccessToken = '<Your access token here>';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
